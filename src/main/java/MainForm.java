62a1b4b0e2255f2569a3b59eb8e19ea1460c0428
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-04-03 21:39:06 +0300

    0.0.2 create table for device and more improvements

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -1,14 +1,24 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JMenuBar;
+import javax.swing.JScrollPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTable;
+import javax.swing.JTabbedPane;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MainForm {
     public final int WIDTH = 600;
     public final int HEIGHT = 800;
 
     public void createForm() {
-        JFrame frame = new JFrame("Main");
+        JFrame frame = new JFrame("Simple NetWorkManager");
         JScrollPane jScrollPane = new JScrollPane();
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         creatMenuBar(frame);
         createTabbedPane(frame, jScrollPane);
@@ -16,41 +26,73 @@ public class MainForm {
         frame.setSize(HEIGHT, WIDTH);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                int x = JOptionPane.showConfirmDialog(
+                        null,
+                        "Are you sure you want to exit NTM?",
+                        "Confirm Exit", JOptionPane.YES_NO_OPTION);
+
+                if(x == JOptionPane.YES_OPTION) {
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                }else{
+                    frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                }
+            }
+        });
     }
 
     // Private section
 
     private void creatMenuBar(JFrame frame) {
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Menu");
+        JMenu menu = new JMenu("NTM");
 
         menuBar.add(menu);
 
-        menu.add(new JMenuItem("Menu 1"));
+        menu.add(new JMenuItem("Settings"));
         menu.addSeparator();
 
-        menu.add(new JMenuItem("Menu 2"));
+        menu.add(new JMenuItem("Hide NTM"));
         menu.addSeparator();
-
-        menu.add(new JMenuItem("Menu 3"));
 
         menuBar.add(menu);
         frame.setJMenuBar(menuBar);
     }
 
 
-    private JScrollPane createTable(JScrollPane jScrollPane) {
+
+
+    private JScrollPane createTableUsage(JScrollPane jScrollPane) {
 
         String[] columnNames = {
                 "Apps",
-                "Download Kb/s",
-                "Upload Kb/s",
+                "Hosts",
+                "Traffic Type",
         };
 
         Object[][] data = {
-                {"Telegram", null, null },
-                {"Chrome", null, null },
-                {"Discord", null, null },
+                {null, null, null },
+                {null, null, null },
+                {null, null, null },
+        };
+
+        return new JScrollPane(new JTable(data, columnNames));
+    }
+
+    private JScrollPane createTableThings(JScrollPane jScrollPane) {
+
+        String[] columnNames = {
+                "Device",
+                "Info",
+                "First Seen",
+        };
+
+        Object[][] data = {
+                {null, null, null },
+                {null, null, null },
+                {null, null, null },
         };
 
         return new JScrollPane(new JTable(data, columnNames));
@@ -59,9 +101,9 @@ public class MainForm {
     private void createTabbedPane(JFrame frame, JScrollPane jScrollPane){
         JTabbedPane tabbedPane = new JTabbedPane();
 
-        tabbedPane.addTab("Graph", createTable(jScrollPane));
-        tabbedPane.addTab("Network", makeTextPanel("void"));
-        tabbedPane.addTab("Usage", null);
+        tabbedPane.addTab("Graph",  makeTextPanel("void"));
+        tabbedPane.addTab("Usage", createTableUsage(jScrollPane));
+        tabbedPane.addTab("Things", createTableThings(jScrollPane));
 
         frame.add(tabbedPane);
 
@@ -74,6 +116,5 @@ public class MainForm {
         panel.add(filler);
         return panel;
     }
-
 
 }
